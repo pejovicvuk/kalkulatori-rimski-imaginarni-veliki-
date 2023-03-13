455d5325b9b706de83261952397afbce86fa5c8a
Pejovic Vuk <pejovic.vuki@gmail.com>
2023-03-13 16:35:09 +0100

    gotovo v3.0

--- kalkulatori (rimski, imaginarni, veliki)/Form4.cs
+++ kalkulatori (rimski, imaginarni, veliki)/Form4.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Singulink.Numerics;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -9,7 +10,7 @@ using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Numerics;
+using static Singulink.Numerics.BigDecimal;
 
 namespace kalkulatori__rimski__imaginarni__veliki_
 {
@@ -404,38 +405,12 @@ namespace kalkulatori__rimski__imaginarni__veliki_
 
             return sb.ToString();
         }
-        public static string Kolicnik(string s1, string s2, int brojDecimala)
+        public static string Kolicnik(string s1, string s2)
         {
-            try
-            {
-                int scale = brojDecimala;
-                BigInteger d1 = BigInteger.Parse(s1.Replace(".", ""));
-                BigInteger d2 = BigInteger.Parse(s2.Replace(".", ""));
-                BigInteger rez = BigInteger.Divide(d1, d2);
-                BigInteger ostatak = BigInteger.Remainder(d1, d2);
-                string rezString = rez.ToString();
-                string ostatakString = ostatak.ToString().PadLeft(scale, '0');
-                string tacka = ".";
-                if (scale > 0)
-                {
-                    rezString += tacka;
-                    for (int i = 0; i < scale; i++)
-                    {
-                        ostatak *= 10;
-                        BigInteger digit = BigInteger.Divide(ostatak, d2);
-                        rezString += digit.ToString();
-                        ostatak = BigInteger.Remainder(ostatak, d2);
-                    }
-                }
-                return rezString;
-            }
-            catch (System.DivideByZeroException)
-            {
-                MessageBox.Show("ne moze deljenje sa nulom");
-                return string.Empty;
-            }
+            BigDecimal x = BigDecimal.Parse(s1);
+            BigDecimal y = BigDecimal.Parse(s2);
+            return BigDecimal.Divide(x, y, 20).ToString();
         }
-
 
         private void buttonPlus_Click(object sender, EventArgs e)
         {
@@ -449,12 +424,11 @@ namespace kalkulatori__rimski__imaginarni__veliki_
 
         private void buttonDeljenje_Click(object sender, EventArgs e)
         {
-            textBoxRezultat.Text = Kolicnik(textBoxPrvi.Text, textBoxDrugi.Text, 50);
+            textBoxRezultat.Text = Kolicnik(textBoxPrvi.Text, textBoxDrugi.Text);
         }
         private void buttonMnozenje_Click(object sender, EventArgs e)
         {
             textBoxRezultat.Text = Proizvod(textBoxPrvi.Text, textBoxDrugi.Text);
         }
-
     }
 }
